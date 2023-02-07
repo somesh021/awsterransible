c7fde1c07b19a5ac6e0e0040909b4cae80add9eb
somesh021 <82119576+somesh021@users.noreply.github.com>
2023-02-07 02:04:20 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -6,15 +6,42 @@ provider "aws" {
   region = "ca-central-1"
   alias = "central"
 }
+resource "aws_security_group" "example_firstregion" {
+  name        = "example_security_group"
+  description = "Allow SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "example_secondregion" {
+  name        = "example_security_group"
+  description = "Allow SSH access"
+  provider = aws.central
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ec2_firstregion" {
    ami           = "ami-00874d747dde814fa"
    instance_type = "t2.micro"
+   vpc_security_group_ids = [aws_security_group.example_firstregion.id]
+   key_name = "terraform-ansible"
    count = 5
 }
 
 resource "aws_instance" "ec2_secondregion" {
   ami = "ami-0dae3a932d090b3de"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example_secondregion.id]
+  key_name = "ansible-terraform"
   count = 5
   provider = aws.central
 }
